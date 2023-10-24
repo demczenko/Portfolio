8d13328278a14c11ad6d1f7e56e22b43563c06c3
demczenko <demczenko@beliani.co.uk>
2023-10-24 12:44:15 +0200

    portfolio started/magnetic icons created

---
+++ src/components/ui/Magnetic.jsx
@@ -0,0 +1,47 @@
+import { motion, useSpring } from "framer-motion";
+import { useRef } from "react";
+
+const spring = {
+  stiffness: 150,
+  damping: 15,
+  mass: 0.1,
+};
+
+const Magnetic = ({ children }) => {
+  const ref = useRef();
+
+  const mousePosition = {
+    x: useSpring(0, spring),
+    y: useSpring(0, spring),
+  };
+
+  const handleMouse = (e) => {
+    const { clientX, clientY } = e;
+    const { height, width, left, top } = ref.current.getBoundingClientRect();
+    const middleX = clientX - (width / 2 + left);
+    const middleY = clientY - (height / 2 + top);
+    mousePosition.x.set(middleX);
+    mousePosition.y.set(middleY);
+  };
+
+  const reset = () => {
+    mousePosition.x.set(0);
+    mousePosition.y.set(0);
+  };
+
+  const { x, y } = mousePosition;
+
+  return (
+    <motion.div
+      style={{ position: "relative", x, y }}
+      onMouseMove={handleMouse}
+      onMouseLeave={reset}
+      ref={ref}>
+      <div className="hover:h-32 hover:w-32 hover:bg-red-200 cursor-pointer flex items-center justify-center">
+        {children}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Magnetic;
